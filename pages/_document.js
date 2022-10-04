6d7ae3e035a9ac894455edadba33a0bedf77c1cc
jul <louis.jungo@ti8m.ch>
2022-10-04 10:47:23 +0200

    icon size

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/logos/maskable_icon_x96.png"></link>
+          <link rel="apple-touch-icon" href="/logos/maskable_icon_x384.png"></link>
           <meta name="theme-color" content="#0c0b15" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         </Head>
