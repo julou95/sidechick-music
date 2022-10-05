00b246cb6f3a1910bed39cfe4de2bf38a109145f
jul <louis.jungo@ti8m.ch>
2022-10-05 23:27:33 +0200

    env

--- app/constants/useOneSignal.js
+++ app/constants/useOneSignal.js
@@ -4,10 +4,9 @@ const useOneSignal = () =>
   useEffect(() => {
     window.OneSignal = window.OneSignal || [];
     OneSignal.push(function () {
-      console.log('LJ - ', '???');
       OneSignal.init({
-        appId: '1184d288-90ec-426f-b29e-a101280b6fd3',
-        safari_web_id: "web.onesignal.auto.087eca46-faed-450d-af5b-90e7f525c88c",
+        appId: process.env.OS_ID,
+        safari_web_id: process.env.OS_WEB_ID,
         notifyButton: {
           enable: true,
           displayPredicate: function() {
