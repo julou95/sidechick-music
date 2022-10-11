27dbcc094b4425fdcfddca9eeabc6121d93d169d
jul <louis.jungo@ti8m.ch>
2022-10-11 10:20:19 +0200

    add songs

--- pages/add.js
+++ pages/add.js
@@ -5,59 +5,87 @@ import { db, storage } from '@/constants/firebaseConfig'
 export default function Add() {
   const [showModal, setShowModal] = useState(false)
   const [hasError, setHasError] = useState(false)
+  const [durationStr, setDurationStr] = useState()
   const songRef = useRef()
-  const idRef = useRef()
   const titleRef = useRef()
   const typeRef = useRef()
   const lyricsRef = useRef()
-  const durationRef = useRef()
   const bpmRef = useRef()
   const noteRef = useRef()
-  const dateRef = useRef()
+
+  const str_pad_left = (string,pad,length) => {
+    return (new Array(length+1).join(pad)+string).slice(-length);
+  }
 
   const save = () => {
     const file = songRef.current.files[0]
     const refValues = [
-      idRef.current.value,
       titleRef.current.value,
       typeRef.current.value,
       file?.name || '',
       lyricsRef.current.value,
-      durationRef.current.value,
-      bpmRef.current.value,
-      noteRef.current.value,
-      dateRef.current.value,
     ]
 
     const hasEmpty = refValues.some(value => !value);
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth()+1;
+    const year = date.getFullYear().toString();
+    const dateStr = str_pad_left(day, '0', 2)+'.'+str_pad_left(month, '0', 2)+'.'+year.substring(2)
 
     if (!hasEmpty) {
-      storage().ref(file.name).put(file)
-      db().collection('lyrics').doc(idRef.current.value).set({
-        id: idRef.current.value,
-        title: titleRef.current.value,
-        type: typeRef.current.value,
-        media: file.name,
-        text: lyricsRef.current.value,
-        duration: durationRef.current.value,
-        bpm: bpmRef.current.value,
-        node: noteRef.current.value,
-        date: dateRef.current.value,
-      }).then(() => {
-        idRef.current.value = ''
-        titleRef.current.value = ''
-        typeRef.current.value = ''
-        songRef.current.value = ''
-        lyricsRef.current.value = ''
-        durationRef.current.value = ''
-        bpmRef.current.value = ''
-        noteRef.current.value = ''
-        dateRef.current.value = ''
+      storage().ref(file.name).put(file).then((res) => {
+        db().collection('lyrics').doc(file.name.split('.')[0]).set({
+          id: file.name.split('.')[0],
+          title: titleRef.current.value,
+          type: typeRef.current.value,
+          media: file.name,
+          text: lyricsRef.current.value,
+          duration: durationStr,
+          bpm: bpmRef.current.value || '-',
+          note: noteRef.current.value || '-',
+          date: dateStr,
+        }).then(() => {
+          titleRef.current.value = ''
+          typeRef.current.value = ''
+          songRef.current.value = ''
+          lyricsRef.current.value = ''
+          bpmRef.current.value = ''
+          noteRef.current.value = ''
+          setHasError(false)
+        }).catch(err => {
+          console.log('LJ - ', 'error', err);
+        })
+
+      }).catch((err) => {
+        console.log('LJ - ', 'error', err);
       })
     } else {
       setHasError(true)
     }
     setShowModal(true)
+  }
+
+  const loadAudio = () => {
+    const file = songRef.current.files[0]
+    const reader = new FileReader();
+    console.log('LJ - file', file);
+
+    reader.onload = async function (event) {
+      const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+      await audioContext.decodeAudioData(event.target.result, function(buffer) {
+        // Obtain the duration in seconds of the audio file (with milliseconds as well, a float value)
+        const duration = buffer.duration;
+        const minutes = Math.floor(duration / 60);
+        const seconds = Math.floor(duration % 60);
+        setDurationStr(str_pad_left(minutes,'0',2)+':'+str_pad_left(seconds,'0',2))
+      });
+    }
+
+    reader.onerror = function (event) {
+      console.error("An error ocurred reading the file: ", event);
+    };
+    reader.readAsArrayBuffer(file);
   }
 
   return (
@@ -68,9 +96,7 @@ export default function Add() {
       <h1><span>New Song</span></h1>
       <div className={styles.addForm}>
         <label>Song</label>
-        <input className={hasError && !songRef.current.value ? styles.error : ''} ref={songRef} type="file" accept='audio/*' />
-        <label>ID:</label>
-        <input className={hasError && !idRef.current.value ? styles.error : ''} ref={idRef} type="text" />
+        <input className={hasError && !songRef.current.value ? styles.error : ''} ref={songRef} type="file" accept='audio/*' onChange={loadAudio} />
         <label>Title:</label>
         <input className={hasError && !titleRef.current.value ? styles.error : ''} ref={titleRef} type="text" />
         <label>Type:</label>
@@ -83,20 +109,14 @@ export default function Add() {
         <textarea ref={lyricsRef} defaultValue="..." />
         <div className={styles.row}>
           <div>
-            <label>Duration:</label>
-            <input className={hasError && !durationRef.current.value ? styles.error : ''} ref={durationRef} type="text" />
-          </div>
-          <div>
             <label>BPM:</label>
-            <input className={hasError && !bpmRef.current.value ? styles.error : ''} ref={bpmRef} type="text" />
+            <input ref={bpmRef} type="text" />
           </div>
           <div>
             <label>Note:</label>
-            <input className={hasError && !noteRef.current.value ? styles.error : ''} ref={noteRef} type="text" />
+            <input ref={noteRef} type="text" />
           </div>
         </div>
-        <label>Date:</label>
-        <input className={hasError && !dateRef.current.value ? styles.error : ''} ref={dateRef} type="text" />
         <button className={styles.saveButton} onClick={save}>SAVE</button>
       </div>
     </>
